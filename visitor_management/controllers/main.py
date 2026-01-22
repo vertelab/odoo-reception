84a95de3e6fb5584d299b3c73290d59cfa303135
marcuszxc <marcus.karlsson@vertel.se>
2026-01-22 10:20:13 +0000

    visitor_management: Added some code for setting the default language

--- visitor_management/controllers/main.py
+++ visitor_management/controllers/main.py
@@ -28,11 +28,14 @@ class reception(http.Controller):
             return False
 
     @http.route('/kiosk/<int:reception_id>/<string:token>', type='http', auth='public', website=True)
-    def launch_reception(self, reception_id, token, lang='en_US'):
+    def launch_reception(self, reception_id, token, lang='sv_SE'):
         _logger.warning(f"Hello {reception_id} {token}")
 
         reception = request.env['reception.reception'].sudo().browse(reception_id)
         
+        if request.env.uid:
+            lang = request.env.user.lang
+
         # return request.render('visitor_management.reception', {
         #     'reception': reception,
         #     'is_mobile': False,
